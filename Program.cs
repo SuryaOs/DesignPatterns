69cb9c002f1bb2c82c5c8018336c90625cf8880a
Surya <ossurya97@gmail.com>
2025-08-26 23:54:28 +0530

    clean up

--- Program.cs
+++ Program.cs
@@ -1,38 +1,38 @@
 using Creational;
-// #region Singleton
-// Console.WriteLine("----------Singleton---------");
-// var logger = Logger.instance;
-// logger.Log("Hello World");
-// #endregion
+#region Singleton
+Console.WriteLine("----------Singleton---------");
+var logger = Logger.instance;
+logger.Log("Hello World");
+#endregion
 
-// #region Factory
-// #region Simple
-// Console.WriteLine("----------Factory---------");
-// var vehicle = new SimpleFactory();
-// var car = vehicle.CreateVehicle("Car");
-// car.Drive();
-// var bike = vehicle.CreateVehicle("Bike");
-// bike.Drive();
-// #endregion
+#region Factory
+#region Simple
+Console.WriteLine("----------Factory---------");
+var vehicle = new SimpleFactory();
+var car = vehicle.CreateVehicle("Car");
+car.Drive();
+var bike = vehicle.CreateVehicle("Bike");
+bike.Drive();
+#endregion
 
-// #region Factory
-// Console.WriteLine("----------Factory Method---------");
-// VehicleFactory cF = new CarFactory();
-// cF.Drive();
-// cF = new BikeFactory();
-// cF.Drive();
-// #endregion
+#region Factory
+Console.WriteLine("----------Factory Method---------");
+VehicleFactory cF = new CarFactory();
+cF.Drive();
+cF = new BikeFactory();
+cF.Drive();
+#endregion
 
-// #region Abstract Factory
-// Console.WriteLine("----------Abstract Factory---------");
-// VehiclesFactory vF = new CarsFactory();
-// Vehicle vH = new(vF);
-// vH.Run();
-// vF = new BikesFactory();
-// vH = new(vF);
-// vH.Run();
-// #endregion
-// #endregion
+#region Abstract Factory
+Console.WriteLine("----------Abstract Factory---------");
+VehiclesFactory vF = new CarsFactory();
+Vehicle vH = new(vF);
+vH.Run();
+vF = new BikesFactory();
+vH = new(vF);
+vH.Run();
+#endregion
+#endregion
 
 #region Builder
 Console.WriteLine("----------Builder---------");
@@ -40,8 +40,6 @@ IBurgerBuilder builder = new BurgerBuilder();
 BurgerDirector director = new BurgerDirector(builder);
 Burger cheeseBurger = director.MakeCheeseBurger();
 Console.WriteLine(cheeseBurger);
-
-
 
 // Custom Build
 Burger burger = builder.AddCheese("Doube Extra Cheese")
